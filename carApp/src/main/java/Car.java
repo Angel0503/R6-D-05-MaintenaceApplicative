f3b4fedd1b020aabb121cf1546075daa11e4a467
Angel0503 <angel.ag64100@gmail.com>
2024-03-08 09:15:30 +0100

    Refactor : ajout des commentaires

--- carApp/src/main/java/Car.java
+++ carApp/src/main/java/Car.java
@@ -1,20 +1,51 @@
+
+/**
+ * Car class.
+ */
 public class Car {
+    /**
+     * Cars acceleration value.
+     */
     public static final int ACCELERATION = 10;
+    /**
+     * Car's max speed.
+     */
     public static final int MAX_SPEED = 120;
+    /**
+     * Car's model.
+     */
     private String model;
+    /**
+     * Car's color.
+     */
     private String color;
+    /**
+     * Car's speed.
+     */
     private int speed;
 
+    /**
+     * Car constructor method.
+     * @param model Car's model
+     * @param color Car's color
+     */
     public Car(String model, String color) {
         this.model = model;
         this.color = color;
         this.speed = 0;
     }
 
+    /**
+     * Get the car's speed.
+     * @return Car's speed
+     */
     public int getSpeed() {
         return this.speed;
     }
 
+    /**
+     * Make the car accelerate.
+     */
     public void accelerate() {
         if (speed + ACCELERATION <= MAX_SPEED) {
             speed += ACCELERATION;
@@ -25,12 +56,18 @@ public class Car {
         }
     }
 
+    /**
+     * Show the details of the car.
+     */
     private void showDetails() {
         System.out.println("Modèle : " + model);
         System.out.println("Couleur : " + color);
         System.out.println("Vitesse actuelle : " + speed);
     }
 
+    /**
+     * Make the car slow down.
+     */
     public void slowDown() {
         if (speed - ACCELERATION >= 0) {
             speed -= ACCELERATION;
@@ -41,6 +78,10 @@ public class Car {
         }
     }
 
+    /**
+     * Start the car.
+     * @param driver Car's driver
+     */
     public void start(Driver driver) {
         if (driver.isAdult()) {
             System.out.println(driver.getName() + " démarre la voiture.");
@@ -49,10 +90,19 @@ public class Car {
         }
     }
 
+    /**
+     * Stop the car.
+     * @param driver Car's driver
+     */
     public void stop(Driver driver) {
         System.out.println(driver.getName() + " arrête la voiture.");
     }
 
+    /**
+     * Change the car's speed.
+     * @param newSpeed Car's new speed
+     * @param driver Car's driver
+     */
     public void changeSpeed(int newSpeed, Driver driver) {
         System.out.println(driver.getName() + " change la vitesse de la voiture à " + newSpeed);
         if (getSpeed() >= newSpeed) {
--- carApp/src/main/java/Driver.java
+++ carApp/src/main/java/Driver.java
@@ -1,17 +1,42 @@
+
+/**
+ * Driver class.
+ */
 public class Driver {
+    /**
+     * Driver's adulthood age value.
+     */
     public static final int ADULTHOOD = 18;
+    /**
+     * Driver's name.
+     */
     private String name;
+    /**
+     * Driver's age.
+     */
     private int age;
 
+    /**
+     * Driver's constructor method.
+     * @param name Driver's name
+     * @param years Driver's age
+     */
     public Driver(String name, int years) {
         this.name = name;
         this.age = years;
     }
 
+    /**
+     * Check if driver is adult.
+     * @return True if driver's age > ADULTHOOD else False
+     */
     public boolean isAdult() {
         return age >= ADULTHOOD;
     }
 
+    /**
+     * Get driver's name.
+     * @return Driver's name
+     */
     public String getName() { return this.name; }
-
 }
